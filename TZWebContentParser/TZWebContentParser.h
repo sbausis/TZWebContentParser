1c96911e3704165268058c9300df318d3f4550dd
Simon Baur <simonbaur@Simons-Mac-Pro.local>
2013-12-08 20:39:07 +0100

    Initial Commit

---
+++ TZWebContentParser/TZWebContentParser.h
@@ -0,0 +1,13 @@
+//
+//  TZWebContentParser.h
+//  TZWebContentParser
+//
+//  Created by Simon Baur on 12/8/13.
+//  Copyright (c) 2013 Simon Baur. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TZWebContentParser : NSObject
+
+@end
---
+++ TZWebContentParser/TZWebContentParser.m
@@ -0,0 +1,13 @@
+//
+//  TZWebContentParser.m
+//  TZWebContentParser
+//
+//  Created by Simon Baur on 12/8/13.
+//  Copyright (c) 2013 Simon Baur. All rights reserved.
+//
+
+#import "TZWebContentParser.h"
+
+@implementation TZWebContentParser
+
+@end
